5dc8da578a28871ead31639c2d11416ff2881d8e
Peter Slesarew <peter.slesarew@babylonhealth.com>
2019-10-03 00:16:49 +0200

    adding tabs stack

--- app/src/main/java/com/slesarew/bottomnavigation/main/GraphPerTabActivity.kt
+++ app/src/main/java/com/slesarew/bottomnavigation/main/GraphPerTabActivity.kt
@@ -7,12 +7,13 @@ import androidx.navigation.findNavController
 import com.slesarew.bottomnavigation.R
 import com.slesarew.bottomnavigation.extension.hideAllNavHostsExcept
 import com.slesarew.bottomnavigation.extension.setupActionBarWithNavController
+import java.util.Stack
 import kotlinx.android.synthetic.main.activity_graph_per_tab.bottom_navigation as bottomNavigation
 
 class GraphPerTabActivity : AppCompatActivity() {
 
     @IdRes
-    private var currentTabId = R.id.home_tab
+    private var tabIds = Stack<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +22,7 @@ class GraphPerTabActivity : AppCompatActivity() {
         /**
          * Showing home tab as first.
          */
-        showNavHost(currentTabId)
+        showNavHost(R.id.home_tab)
 
         /**
          * Wiring bottom navigation click with navigation.
@@ -29,19 +30,34 @@ class GraphPerTabActivity : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemSelectedListener { showNavHost(it.itemId).let { true } }
     }
 
-    override fun onBackPressed() =
-        findNavController(currentTabId)
+    /**
+     * Function is responsible for managing graphs back stack properly.
+     * 1. It is popping back stack of particular graph.
+     * 2. When particular graph's back stack is empty it pop a tab and select the youngest one in the stack.
+     * 3. When tabs stack is empty it delegates on back press to the system.
+     */
+    override fun onBackPressed() {
+        findNavController(tabIds.peek())
             .popBackStack()
             .not()
-            .let { shouldClose ->
-                if (shouldClose) super.onBackPressed()
+            .also { shouldClose ->
+                if (shouldClose) {
+                    tabIds.pop()
+                    if (tabIds.isEmpty()) super.onBackPressed() else selectYoungestTab()
+                }
             }
+    }
+
+    /**
+     * Function pops the youngest tab and select it. It will bottom navigation will automatically call
+     * setOnNavigationItemSelectedListener and out showNavHost().
+     */
+    private fun selectYoungestTab() = tabIds.pop().also { bottomNavigation.selectedItemId = it }
 
     /**
      * Setup code. Taken from documentation. ¯\_(ツ)_/¯
      */
-    override fun onSupportNavigateUp(): Boolean =
-        findNavController(currentTabId).navigateUp()
+    override fun onSupportNavigateUp(): Boolean = findNavController(tabIds.peek()).navigateUp()
 
     /**
      * Function is responsible for switching content between tabs.
@@ -50,10 +66,8 @@ class GraphPerTabActivity : AppCompatActivity() {
      * 2. Finding graph by tab id and update ActionBar with it.
      * 3. Hiding all graphs except new current tab id.
      */
-    private fun showNavHost(@IdRes tabId: Int) {
-        currentTabId = tabId
-
-        setupActionBarWithNavController(currentTabId)
-        hideAllNavHostsExcept(currentTabId)
+    private fun showNavHost(@IdRes tabId: Int) = tabIds.push(tabId).also {
+        setupActionBarWithNavController(it)
+        hideAllNavHostsExcept(it)
     }
 }
